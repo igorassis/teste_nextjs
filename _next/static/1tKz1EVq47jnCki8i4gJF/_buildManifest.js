be1e1da6d2c2a7ca465d928c286c8ee72323cfe8
Igor de Assis Pires <igor.pires@fretebras.com.br>
2020-05-07 15:40:05 -0300

    Deploy gh-pages

--- _next/static/1tKz1EVq47jnCki8i4gJF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/login":[a]}}("static\u002Fchunks\u002F14dc8b4f3b125c47894f7039174deb040089f663.f8d1b9691988d46374df.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
