fa7616c0e55d6fd7569a5781c40e36ef31dd089e
Igor de Assis Pires <igor.pires@fretebras.com.br>
2020-05-07 16:26:32 -0300

    Removed OUT

--- out/_next/static/8UMYdhMr5cd9maeZPP1GD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/login":[a]}}("static\u002Fchunks\u002F8f26012763722df5a0e4b6eeeb0a08108520dc34.f8d1b9691988d46374df.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
